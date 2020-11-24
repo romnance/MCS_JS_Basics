6f02dfc9c465b064a6bf12926f991f7160197d09
Anastasia <anromanova.design@gmail.com>
2020-11-24 19:03:57 +0100

    Praktika3

---
+++ Praktika/js/script.js
@@ -0,0 +1,16 @@
+let totalCash = prompt('How much cash do you got?');
+Number(totalCash);
+
+let watchesCount = prompt('How many watches you want?');
+Number(watchesCount);
+
+let earringsCount = prompt('How many pairs of earrings you want?');
+Number(earringsCount);
+
+let watchesSum = Number(prompt('How much does one watch cost?'))*watchesCount;
+
+let earringsSum = Number(prompt('How much does one pair of earrings cost?'))*earringsCount;
+
+let answer = totalCash > earringsSum + watchesSum;
+
+document.body.innerHTML="<h1>" + answer + "</h1>";
