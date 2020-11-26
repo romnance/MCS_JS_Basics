a26d0d74c3c3be6efd5a8af83d4c0be1298f14e1
Anastasia <anromanova.design@gmail.com>
2020-11-26 13:22:48 +0100

    upd

--- Praktika3/js/script.js
+++ Praktika3/js/script.js
@@ -1,5 +1,5 @@
-const Page = document.querySelector('.page');
-Page.style.color = 'ghostwhite';
+const page = document.querySelector('.page');
+page.style.color = 'ghostwhite';
 
 document.body.style.background = "#696969";
 
@@ -9,5 +9,5 @@ span.innerText += 'DJ Rashad';
 const image = document.querySelector('img');
 image.setAttribute('src','http://www.rokolectiv.ro/archive/2012/content/6.Day4/0.DJRashad/rashad.jpg');
 
-const ShortBio = document.querySelector('.short-bio');
-ShortBio.className += ' animated';
+const shortBio = document.querySelector('.short-bio');
+shortBio.className += ' animated';
