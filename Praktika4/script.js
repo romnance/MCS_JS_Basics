f5ccc9cf0c01ca4349717d1c8a77e7c9ea494e8b
Anastasia <anromanova.design@gmail.com>
2020-12-01 12:31:12 +0100

    praktika4-2

--- Praktika4/script.js
+++ Praktika4/script.js
@@ -1,7 +1,5 @@
 let num = Number(prompt("Please, enter any number here"));
 
-if(num<=0) {
-    console.log(Math.abs(num));
-}else if(num>=0) {
-	console.log(num);
-}
+if (num <= 0) {console.log(Math.abs(num));} 
+
+else if(num >= 0) {console.log(num);}
