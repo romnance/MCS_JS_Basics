93623810b6420b574524ac0402e47246235eb6d9
Anastasia <anromanova.design@gmail.com>
2020-12-03 17:16:50 +0100

    Thesinevalue

---
+++ Praktika5/form/js/script.js
@@ -0,0 +1,8 @@
+
+const form = document.forms[0];
+const insert = document.querySelector('.total');
+
+form.onsubmit = function(e) {
+	e.preventDefault();
+	insert.innerHTML = Math.sin(form.elements.ugol.value);
+}
