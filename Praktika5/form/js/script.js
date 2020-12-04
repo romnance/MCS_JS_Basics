547b3d44e0a1e312e069e4175b6944ba2ae0f096
Anastasia <anromanova.design@gmail.com>
2020-12-04 10:12:53 +0100

    en

--- Praktika5/form/js/script.js
+++ Praktika5/form/js/script.js
@@ -4,6 +4,6 @@ const insert = document.querySelector('.total');
 
 form.onsubmit = function(e) {
 	e.preventDefault();
-	insert.innerHTML = Math.sin(form.elements.ugol.value * Math.PI / 180); //высчитывает в градусах
+	insert.innerHTML = Math.sin(form.elements.ugol.value * Math.PI / 180); //calculates in degrees
 }
 
