e384acd5ab2162d115411bcbcc3f296d870415a1
Anastasia <anromanova.design@gmail.com>
2020-12-08 16:24:10 +0100

    fix

--- praktika6.4.1/js/script.js
+++ praktika6.4.1/js/script.js
@@ -1,19 +1,17 @@
 const APIkey = 'b6e3fffce345ff642781561151cc05ea';
 const form = document.forms[0];
-const city = 'Milan'; // значением city должен быть город из инпута city form.elemnts.city.value
-const url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIkey;
-
-
 const insert = document.querySelector('.degrees');
-
-let xhr = new XMLHttpRequest();
-
-xhr.open('GET', url, false); 
-
-xhr.send();
 
 form.onsubmit = function(e) {
 	e.preventDefault();
+	var city = form.elements.city.value;
+	const url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIkey;
+	let xhr = new XMLHttpRequest();
+
+	xhr.open('GET', url, false); 
+
+	xhr.send();
+
 	if(xhr.status != 200) {
 		console.log(xhr.status +' '+ xhr.statusText);
 	} else {
@@ -22,6 +20,9 @@ form.onsubmit = function(e) {
 		insert.innerHTML = DATA.main.temp - 273;
 	} 
 }
+
+
+
 
 
 
